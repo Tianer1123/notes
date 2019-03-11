3e474dc7ea446d4859c7dd9c2307fa13b40cee2b
Tianer1123 <q219833@126.com>
2019-03-11 17:06:44 +0800

    k-近邻算法实例

--- machineLearinginAction/src/cp2/kNN.py
+++ machineLearinginAction/src/cp2/kNN.py
@@ -1,6 +1,9 @@
 from numpy import *
 import operator
 
+import matplotlib
+import matplotlib.pyplot as plt
+
 def createDataSet():
     group = array([[1.0, 1.1], [1.0, 1.1], [0, 0], [0, 0.1]])
     labels = ['A', 'A', 'B', 'B']
@@ -21,7 +24,6 @@ def classify0(inX, dataSet, labels, k):
     sqDiffMat = diffMat**2
     sqDistances = sqDiffMat.sum(axis=1)
     distances = sqDistances**0.5
-    print(distances)
 
     sortedDistIndicies = distances.argsort()  # 返回数组从下到大的索引值
     classCount = {}
@@ -33,6 +35,58 @@ def classify0(inX, dataSet, labels, k):
     sortedClassCount = sorted(classCount.items(), key=operator.itemgetter(1), reverse=True)
     return sortedClassCount[0][0]  # 返回频率最高的标签(分类)
 
+def file2matrix(filename):
+    fr = open(filename)
+    arrayOlines = fr.readlines()
+    numberOfLines = len(arrayOlines)
+    returnMat = zeros((numberOfLines, 3))
+    classLabelVector = []
+    index = 0
 
-group, labels = createDataSet()
-print(classify0([0, 0], group, labels, 3))
+    for line in arrayOlines:
+        line = line.strip()
+        listFromLine = line.split('\t')
+        returnMat[index:] = listFromLine[0:3]
+        classLabelVector.append(int(listFromLine[-1]))
+        # classLabelVector.append(listFromLine[-1])
+        index += 1
+
+    return returnMat, classLabelVector
+
+def autoNorm(dataSet):
+    minVals = dataSet.min(0)
+    maxVals = dataSet.max(0)
+    ranges = maxVals - minVals
+    normDataSet = zeros(shape(dataSet))
+    m = dataSet.shape[0]  # 第一个维度的元素的个数
+    normDataSet = dataSet - tile(minVals, (m, 1))  # 为了能让矩阵相减，使用tile将minVals复制扩展成同dataSet矩阵相同的矩阵
+    normDataSet = normDataSet/tile(ranges, (m, 1))  # tile作用同上
+    return normDataSet, ranges, minVals
+
+def classifyPerson():
+    resultLists = ['not at all', 'in small doses', 'in large doses']
+    percentTats = float(input("percentage of time spent playing video games?"))
+    ffMiles = float(input("frequent flier miles earned per year?"))
+    iceCream = float(input("liters of ice cream consumed per year?"))
+    datingDataMat, datingLabels = file2matrix('datingTestSet2.txt')
+    normMat, ranges, minVals = autoNorm(datingDataMat)
+    inArr = array([ffMiles, percentTats, iceCream])
+    classifierResult = classify0((inArr - minVals)/ranges, normMat, datingLabels, 3)
+    print("You will probably like this person: {}".format(resultLists[classifierResult - 1]))
+
+def datingClassTest():
+    hoRatio = 0.10
+    datingDataMat, datingLabels = file2matrix('datingTestSet2.txt')
+    normMat, ranges, minVals = autoNorm(datingDataMat)
+    m = normMat.shape[0]
+    numTestVecs = int(m*hoRatio)
+    errorCount = 0.0
+    for i in range(numTestVecs):
+        classifierResult = classify0(normMat[i, :], normMat[numTestVecs:m, :], datingLabels[numTestVecs:m], 3)
+        print("the classifier came back with: {}, the real answer is: {}".format(classifierResult, datingLabels[i]))
+        if (classifierResult != datingLabels[i]):
+            errorCount += 1.0
+    print("the total error rate is: {}".format(errorCount/float(numTestVecs)))
+
+
+classifyPerson()
