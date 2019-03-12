5416ee6d676feaafe547aab22bfedf5358778dd5
Tianer1123 <q219833@126.com>
2019-03-12 09:47:06 +0800

    k-近邻算法

--- machineLearinginAction/src/cp2/kNN.py
+++ machineLearinginAction/src/cp2/kNN.py
@@ -1,8 +1,10 @@
 from numpy import *
+from os import listdir
 import operator
 
 import matplotlib
 import matplotlib.pyplot as plt
+
 
 def createDataSet():
     group = array([[1.0, 1.1], [1.0, 1.1], [0, 0], [0, 0.1]])
@@ -35,6 +37,7 @@ def classify0(inX, dataSet, labels, k):
     sortedClassCount = sorted(classCount.items(), key=operator.itemgetter(1), reverse=True)
     return sortedClassCount[0][0]  # 返回频率最高的标签(分类)
 
+
 def file2matrix(filename):
     fr = open(filename)
     arrayOlines = fr.readlines()
@@ -53,6 +56,7 @@ def file2matrix(filename):
 
     return returnMat, classLabelVector
 
+
 def autoNorm(dataSet):
     minVals = dataSet.min(0)
     maxVals = dataSet.max(0)
@@ -62,6 +66,7 @@ def autoNorm(dataSet):
     normDataSet = dataSet - tile(minVals, (m, 1))  # 为了能让矩阵相减，使用tile将minVals复制扩展成同dataSet矩阵相同的矩阵
     normDataSet = normDataSet/tile(ranges, (m, 1))  # tile作用同上
     return normDataSet, ranges, minVals
+
 
 def classifyPerson():
     resultLists = ['not at all', 'in small doses', 'in large doses']
@@ -73,6 +78,7 @@ def classifyPerson():
     inArr = array([ffMiles, percentTats, iceCream])
     classifierResult = classify0((inArr - minVals)/ranges, normMat, datingLabels, 3)
     print("You will probably like this person: {}".format(resultLists[classifierResult - 1]))
+
 
 def datingClassTest():
     hoRatio = 0.10
@@ -89,4 +95,56 @@ def datingClassTest():
     print("the total error rate is: {}".format(errorCount/float(numTestVecs)))
 
 
-classifyPerson()
+# 手写字识别
+def img2vector(filename):
+    """
+    :param filename: 文件名
+    :return: 32 x 32 矩阵 --> 1 x 1024 向量
+    """
+    returnVect = zeros((1, 1024))
+    fr = open(filename)
+    for i in range(32):
+        lineStr = fr.readline()
+        for j in range(32):
+            returnVect[0, 32*i + j] = int(lineStr[j])
+    return returnVect
+
+
+def handwritingClassTest():
+    # 获取目录内容
+    hwLabels = []
+    trainingFileList = listdir('trainingDigits')
+    m = len(trainingFileList)
+    trainingMat = zeros((m, 1024))
+    for i in range(m):
+        fileNameStr = trainingFileList[i]
+        fileStr = fileNameStr.split('.')[0]
+        classNumStr = int(fileStr.split('_')[0])
+        # 保存分类
+        hwLabels.append(classNumStr)
+        # 解析分类数据
+        trainingMat[i, :] = img2vector('trainingDigits/%s' % fileNameStr)
+
+    testFileList = listdir('testDigits')
+    errorCount = 0.0
+    mTest = len(testFileList)
+    for i in range(mTest):
+        fileNameStr = testFileList[i]
+        fileStr = fileNameStr.split('.')[0]
+        classNumStr = int(fileStr.split('_')[0])
+        vectorUnderTest = img2vector('testDigits/%s' % fileNameStr)
+        classifierResult = classify0(vectorUnderTest, trainingMat, hwLabels, 3)
+        print("The classifier came back with: %d, the real answer is: %d" % (classifierResult, classNumStr))
+        if classifierResult != classNumStr:
+            errorCount += 1.0
+    print("\nthe total number of errors is: %d" % errorCount)
+    print("\nthe total error rate is: %f" % (errorCount/float(mTest)))
+
+
+handwritingClassTest()
+
+# classifyPerson()
+# testVector = img2vector('testDigits/0_13.txt')
+# print(testVector[0, 32:63])
+
+
